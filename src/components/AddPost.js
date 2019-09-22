2966b27fb2d7006362d6aca8d8a5396f26323b40
Sandi <sandi.c.gau@gmail.com>
2019-09-21 23:51:45 -0500

    Add validation to form

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -13,7 +13,8 @@ class AddPost extends Component {
             title: '',
             author: '',
             category: '',
-            body: ''
+            body: '',
+            validated: false,
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -31,12 +32,25 @@ class AddPost extends Component {
 
     handleSubmit(event) {
         event.preventDefault();
-        const post = this.state
-        post.timestamp = Date.now();
-        post.id = uuidv4();
-        this.props.addPost(post)
-        this.props.fetchPosts()
-        this.props.history.push("/")
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+            this.setState({ validated: true })
+        } else {
+            const post = {
+                title: this.state.title,
+                author: this.state.author,
+                category: this.state.category,
+                body: this.state.body
+            }
+            post.timestamp = Date.now();
+            post.id = uuidv4();
+            this.props.addPost(post)
+            this.props.fetchPosts()
+            this.props.history.push("/")
+        }
+
     }
 
     render() {
@@ -49,25 +63,37 @@ class AddPost extends Component {
                     <Card style={{ width: '75%' }} className="m-auto">
                         <Card.Body>
                             <Card.Title className="center">Add Post</Card.Title>
-                            <Form style={{ width: '100%' }} onSubmit={this.handleSubmit}>
+                            <Form noValidate validated={this.state.validated} style={{ width: '100%' }} onSubmit={this.handleSubmit}>
                                 <Form.Group controlId="title" value={this.state.title} onChange={this.handleChange}>
                                     <Form.Label>Title</Form.Label>
-                                    <Form.Control type="text" placeholder="Title" />
+                                    <Form.Control type="text" placeholder="Title" required />
+                                    <Form.Control.Feedback type="invalid">
+                                        Title is required.
+                                    </Form.Control.Feedback>
                                 </Form.Group>
                                 <Form.Group controlId="author" value={this.state.author} onChange={this.handleChange}>
                                     <Form.Label>Author</Form.Label>
-                                    <Form.Control type="text" placeholder="Author" />
+                                    <Form.Control type="text" placeholder="Author" required />
+                                    <Form.Control.Feedback type="invalid">
+                                        Author is required.
+                                    </Form.Control.Feedback>
                                 </Form.Group>
                                 <Form.Group controlId="category" onChange={this.handleChange}>
                                     <Form.Label>Category</Form.Label>
-                                    <Form.Control as="select">
-                                        <option>Select Category</option>
+                                    <Form.Control required as="select">
+                                        <option value="">Select Category</option>
                                         {categories}
                                     </Form.Control>
+                                    <Form.Control.Feedback type="invalid">
+                                        Category is required.
+                                    </Form.Control.Feedback>
                                 </Form.Group>
                                 <Form.Group controlId="body" value={this.state.body} onChange={this.handleChange}>
                                     <Form.Label>Post</Form.Label>
-                                    <Form.Control as="textarea" rows="5" placeholder="Write your post" />
+                                    <Form.Control as="textarea" rows="5" placeholder="Write your post" required />
+                                    <Form.Control.Feedback type="invalid">
+                                        Post is required.
+                                    </Form.Control.Feedback>
                                 </Form.Group>
                                 <div className="col text-center">
                                     <Button variant="primary" type="submit">
